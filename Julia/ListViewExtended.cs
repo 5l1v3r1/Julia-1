3e251d6ba80f26acc03ed7a519dfa088c8ac2449
Alfur 'Felt her' Deltur <admin@skid.pro>
2015-06-03 15:41:11 +1200

    Fixed ListView flicker

--- Julia/ListViewExtended.cs
+++ Julia/ListViewExtended.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -167,6 +168,42 @@ namespace Julia
             if (m.Msg != 0x14)
             {
                 base.OnNotifyMessage(m);
+            }
+        }
+
+
+        private bool isInWmPaintMsg = false;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NMHDR
+        {
+            public IntPtr hwndFrom;
+            public IntPtr idFrom;
+            public int code;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0F: // WM_PAINT
+                    this.isInWmPaintMsg = true;
+                    base.WndProc(ref m);
+                    this.isInWmPaintMsg = false;
+                    break;
+                case 0x204E: // WM_REFLECT_NOTIFY
+                    NMHDR nmhdr = (NMHDR)m.GetLParam(typeof(NMHDR));
+                    if (nmhdr.code == -12)
+                    { // NM_CUSTOMDRAW
+                        if (this.isInWmPaintMsg)
+                            base.WndProc(ref m);
+                    }
+                    else
+                        base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
             }
         }
     }
